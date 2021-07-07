81efad818221dd25d022818f7c8fc54c2c5dfbea
Victor Isidoro <victorisidoro@icloud.com>
2021-07-07 12:55:00 +0100

    RQG-5_API training and discovery

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [quiz, setQuiz] = useState([])
+  const [error, setError] = useState(false)
+  useEffect(() => {
+    axios
+      .get(
+        `https://trivia.willfry.co.uk/api/questions?categories=movies,music&limit=5`
+      )
+      .then((quiz) => {
+        setQuiz(quiz.data)
+        setError(false)
+      })
+      .catch((error) => {
+        setQuiz([])
+        setError({ message: 'Error fetching Quiz!' })
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-      
-
-     
-      
-        
-      </header>
+    <div>
+      Quiz data:
+      {quiz ? quiz[0].question : 'not yet'}
     </div>
   )
 }
