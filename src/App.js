a4b560e75a9a36bc20a79450b2acabcc6f5ba9a4
Peter Borrow <borrow.peter83@gmail.com>
2021-07-21 09:41:30 +0100

    FEATURE: Updated progress bar state in app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import { useState, useEffect } from 'react'
 import RenderTime from './components/QuestionTimer'
+import ProgressBar from './components/ProgressBar'
 
 const App = () => {
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((oldValue) => {
+        const newValue = oldValue + 1
+
+        if (newValue === 10) {
+          clearInterval(interval)
+        }
+        return newValue
+      })
+    }, 1000)
+  }, [])
   return (
     <div>
-      <h1>Question Timer</h1>
-
       <div>
         <CountdownCircleTimer
           isPlaying
-          duration={20}
+          duration={10}
           size={60}
           strokeWidth={8}
           colors={[
@@ -18,10 +32,11 @@ const App = () => {
             ['#FFA500', 0.25],
             ['#FF0000'],
           ]}
-          onComplete={() => alert('too late')}
+          //onComplete={() => alert('too late')}
         >
           {RenderTime}
         </CountdownCircleTimer>
+        <ProgressBar value={progress} max={10} />
       </div>
     </div>
   )
