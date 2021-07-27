a41432470ce04a79e49addcbd24960cc60c7b3dd
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-07-27 09:26:06 +0200

    Style updated

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { useState } from 'react'
-
+import { GameDiv } from './GameStyle'
 import QuestionDiv from './Question'
 import RenderTime from './QuestionTimer'
 import { InfoCircleTwoTone } from '@ant-design/icons'
@@ -17,36 +17,34 @@ const Game = ({ completequiz, setShowScore }) => {
     }
   }
   return (
-    <div>
-      <div className="game">
-        <div className="topBar">
-          <CountdownCircleTimer
-            isPlaying
-            duration={10}
-            size={60}
-            strokeWidth={8}
-            colors={[
-              ['#00FF00', 0.25],
-              ['#FFFF00', 0.25],
-              ['#FFA500', 0.25],
-              ['#FF0000'],
-            ]}
-            //onComplete={() => alert('too late')}
-          >
-            {RenderTime}
-          </CountdownCircleTimer>
-          <button>
-            <InfoCircleTwoTone />
-          </button>
-        </div>
-        <QuestionDiv
-          details={completequiz[currentQuestion]}
-          handleAnswer={handleAnswer}
-          progress={currentQuestion + 1}
-          totalQuestion={totalQuestion}
-        />
+    <GameDiv>
+      <div className="topBar">
+        <CountdownCircleTimer
+          isPlaying
+          duration={10}
+          size={60}
+          strokeWidth={8}
+          colors={[
+            ['#00FF00', 0.25],
+            ['#FFFF00', 0.25],
+            ['#FFA500', 0.25],
+            ['#FF0000'],
+          ]}
+          //onComplete={() => alert('too late')}
+        >
+          {RenderTime}
+        </CountdownCircleTimer>
+        <button>
+          <InfoCircleTwoTone />
+        </button>
       </div>
-    </div>
+      <QuestionDiv
+        details={completequiz[currentQuestion]}
+        handleAnswer={handleAnswer}
+        progress={currentQuestion + 1}
+        totalQuestion={totalQuestion}
+      />
+    </GameDiv>
   )
 }
 
