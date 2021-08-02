ddec1e548b5aaf45d02e3396dc30d56d444d5de1
Guilhaxr <guilhaxr123@gmail.com>
2021-08-02 12:17:45 +0100

    more changes in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,32 +2,28 @@
 import PlayGameButton from './PlayGameButton'
 import FindAQuizz from './FindAQuizz'
 import Credits from './Credits'
-import
+import styled from 'styled-components'
 
+const MenuContainer = styled.div`
 
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    margin : 50px 350px 0px;
+    background-color: aqua;
+   
+   
+`
 
 const Menu = () => {
     return (
-        <div>
+        <MenuContainer>
             <img src="/src/images/idea-5393862_640.png " alt="logo" />
             <PlayGameButton />
             <FindAQuizz />
             <Credits />
-        </div>
+        </MenuContainer>
     )
 }
 
 export default Menu
-
-vbackground - color: aqua;
-justify - content: center;
-display: flex;
-flex - direction: column;
-padding - top: 50px;
-padding - left: 50px;
-padding - right: 50px;
-padding - bottom: 50px;
-margin - top: 100px;
-margin - left: 450px;
-margin - right: 450px;
-border - radius: 20px;
