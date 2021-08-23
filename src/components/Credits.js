411ce5b1eb07a25c40974fbad7b3b0af36928c18
Guilhaxr <guilhaxr123@gmail.com>
2021-08-24 00:26:59 +0100

    more changes

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import Styled from "styled-components"
+import DevelopedBy from "../images/DevelopedBy.001.png"
+
+const Container = Styled.fieldset`
+
+width: 100%;
+display: flex;
+flex-direction: column;
+border: 2px solid #ddd;
+border-radius: 10px;
+padding: 20px 70px 50px;
+background: rgba(0, 0, 0, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+@media (max-width: 450px) {
+    font-size: 15px;
+    padding: 10px 30px 40px;
+}
+`
+
+const TitleLegend = Styled.legend`
+text-align: center;
+border: 2px solid #ddd;
+border-radius: 10px;
+background-color: white;
+`
 
 const Credits = () => {
     return (
         <div>
-            <p>helloo there</p>
+            <Container>
+                <TitleLegend>
+                    <ImageLeaderBoard src="../images/DevelopedBy.001.png" alt="leaderboard" />
+                </TitleLegend>
+            </Container>
         </div>
     )
 }
