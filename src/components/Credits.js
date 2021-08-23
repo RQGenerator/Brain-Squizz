1d7d29ce6fcd5617752b41457ad42294e1fdff60
Guilhaxr <guilhaxr123@gmail.com>
2021-08-24 00:04:46 +0100

    changes in route credits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,30 +1,10 @@
-/* eslint-disable prettier/prettier */
-import styled from 'styled-components'
-
-
-const Creditss = styled.button`
-box - shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 30px;
-background: #498FF5;
-font-size: 20px;
-margin: 0px 0px 30px;
-width: 30%;
-
-@media (max-width: 520px) {
-    font-size: 15px;   
-
-}
-
-@media (max-width: 440px) {
-    font-size: 10px;
-
-}
-`
+import React from 'react'
 
 const Credits = () => {
     return (
-        <Creditss >Credits</Creditss>
-
+        <div>
+            <p>helloo there</p>
+        </div>
     )
 }
 
