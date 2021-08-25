fb9926e8bb09cdf860ea6857400c07323f72e28a
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-08-25 08:16:30 +0200

    Shuffle answers function created and implemented Fisher-Yates Shuffle algorithm

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,11 +3,25 @@ import './AnswersStyle'
 import { CheckSquareTwoTone } from '@ant-design/icons'
 import { Button } from 'antd'
 
+const shuffle = (array) => {
+  let currentIndex = array.length
+  while (currentIndex !== 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    let tempValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = tempValue
+  }
+  return array
+}
+
 const QuestionDiv = ({ details, handleAnswer }) => {
+  let answers = [...details.answers]
+  shuffle(answers)
   return (
     <MainGame>
       <p>{details.question.question}</p>
-      {details.answers.map((answer, i) => (
+      {answers.map((answer, i) => (
         <Button
           icon={answer.isCorrect && <CheckSquareTwoTone />}
           key={i}
