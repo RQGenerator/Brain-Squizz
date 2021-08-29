19f3d0aa53313a3703f75ad9504e4d6bd5e5d1a4
Guilhaxr <guilhaxr123@gmail.com>
2021-08-29 18:12:15 +0100

    current score css done

--- src/components/CurrentScore.js
+++ src/components/CurrentScore.js
@@ -1,5 +1,37 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import { Button2 } from './styles'
+import Styled from 'styled-components'
+
+const Container = Styled.div`
+  background-color: rgba(0, 0, 0, 0.25);
+  margin-left: 30px;
+  margin-right: 30px;
+  border-radius: 20px;
+  padding: 15px 50px;
+`
+const Title = Styled.h1`
+  text-align: center;
+  margin-top: 35px;
+  background: #d8d9e8;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 44px;
+  padding: 20px 0px;
+  justify-content: center;
+  display: flex;
+  font-size: 30px;
+`
+
+const Gif = Styled.img`
+width: 500px;
+height: 400px;
+padding: 50px 0px;
+`
+
+const BoxButton = Styled.div`
+display: flex;
+justify-content: flex-end;
+`
 
 const CurrentScore = ({ score, totalScore, result, proceed }) => {
     console.log(result)
@@ -27,12 +59,14 @@ const CurrentScore = ({ score, totalScore, result, proceed }) => {
     //calcular o score total
     const totalPoints = totalScore(result)
     return (
-        <div>
-            <img src={gifURL} alt="gif" />
-            <p>{answerPoints.points} * {answerPoints.bonus}</p>
-            <p>{totalPoints}</p>
-            <button onClick={() => { proceed() }} >continue</button>
-        </div>
+        <Container>
+            <Title>Your Score: {totalPoints}</Title>
+            <Gif src={gifURL} alt="gif" />
+            <p style={{ display: "none" }}>{answerPoints.points} * {answerPoints.bonus}</p>
+            <BoxButton>
+                <Button2 onClick={() => { proceed() }} >Continue</Button2>
+            </BoxButton>
+        </Container>
     )
 }
 
