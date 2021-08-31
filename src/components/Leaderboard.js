4abd612c15af4db7e049e9971003715cb249a66a
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-08-31 21:12:28 +0200

    Leaderboard design finalized

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,72 +1,20 @@
-import Styled from 'styled-components'
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-const Line = Styled.tr`
-display: flex;
-justify-content: space-between;
-font-size: 25px;
-border-bottom: 1px solid #ddd;
-@media (max-width: 450px) {
-    
-    font-size: 15px;
+const callBackendAPI = async () => {
+  const response = await fetch('/api')
+  const body = await response.json()
+
+  if (response.status !== 200) {
+    throw Error(body.message)
+  }
+  return body
 }
-`
 
-const Container = Styled.fieldset`
-width: 100%;
-display: flex;
-flex-direction: column;
-border: 2px solid #ddd;
-border-radius: 10px;
-padding: 20px 70px 50px;
-background: rgba(0, 0, 0, 0.25);
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-@media (max-width: 450px) {
-    font-size: 15px;
-    padding: 10px 30px 40px;
-}
-`
-
-const Content = Styled.td`
-padding: 20px
-`
-
-const Title1 = Styled.th`
-padding: 20px;
-color: ghostwhite;
-`
-
-const Title2 = Styled.th`
-padding: 20px;
-color: ghostwhite;
-`
-
-const Title3 = Styled.th`
-padding: 20px;
-color: ghostwhite;
-`
-
-const TitleContainer = Styled.tr`
-display: flex;
-justify-content: space-between;
-font-size: xx-large;
-@media (max-width: 450px) {    
-    font-size: 15px;
-}
-`
-
-const TitleLegend = Styled.legend`
-text-align: center;
-border: 2px solid #ddd;
-border-radius: 10px;
-background-color: white;
-`
-
-const ImageLeaderBoard = Styled.img`
-width: 50%;
-`
 const Leaderboard = () => {
-  // implement API Call from database to feed leaderboard - backend necessary
-  const playerScore = [
+  const history = useHistory()
+
+  const [playerScore, setPlayerScore] = useState([
     {
       name: 'Jamal',
       score: 20,
@@ -90,30 +38,75 @@ const Leaderboard = () => {
       name: 'Maorbid',
       score: 72,
     },
-  ]
+  ])
+  callBackendAPI()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
   playerScore.sort((a, b) => b.score - a.score)
   return (
-    <div>
-      <Container>
-        <TitleLegend>
-          <ImageLeaderBoard
-            src={process.env.PUBLIC_URL + '/public/images/leaderBoard.png'}
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="flex items-center flex-col bg-gray-200 bg-opacity-30 backdrop-filter backdrop-blur-lg shadow-2xl rounded-3xl w-11/12 h-5/6 md:w-9/12 md:h-5/6">
+        <div className="flex p-5 rounded-2xl bg-white justify-center items-center  lg:-mt-10 -mt-8 shadow-md">
+          <img
+            src={process.env.PUBLIC_URL + '/images/leaderBoard.png'}
             alt="leaderboard"
           />
-        </TitleLegend>
-        <TitleContainer>
-          <Title1>Rank</Title1>
-          <Title2>Name</Title2>
-          <Title3>Score</Title3>
-        </TitleContainer>
-        {playerScore.map((score, i) => (
-          <Line>
-            <Content>#{i + 1}</Content>
-            <Content>{score.name}</Content>
-            <Content>{score.score}</Content>
-          </Line>
-        ))}
-      </Container>
+        </div>
+        <div class="w-5/6 bg-gray-100 border-b-2 border-indigo-600 text-gray-600 uppercase text-sm rounded-t-xl mt-5">
+          <div class="flex justify-around ">
+            <div class="py-3 px-6 text-left w-1/5"></div>
+            <div class="py-3 px-6 text-left"></div>
+            <div class="py-3 px-6 text-center">Name</div>
+            <div class="py-3 px-6 text-center">Points</div>
+            <div class="py-3 px-6 text-center">Difficulty</div>
+          </div>
+        </div>
+        <div className="overflow-auto w-5/6">
+          <table class="min-w-max w-full table-auto rounded-xl">
+            <tbody class="text-gray-600 text-sm font-light">
+              {playerScore.map((score, i) => (
+                <tr class="border-b border-gray-200 bg-gray-200 hover:bg-gray-100">
+                  <td class="py-3 px-6"># {i + 1}</td>
+                  <td class="py-3 px-6">
+                    <div class="flex items-center justify-center">
+                      <img
+                        class="w-12 h-12 rounded-full"
+                        src="https://randomuser.me/api/portraits/men/1.jpg"
+                        alt="avatar"
+                      />
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <div class="flex items-center justify-center">
+                      <span>{score.name}</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <span>{score.score} pts</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <div class="flex item-center justify-center">
+                      <span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">
+                        Active
+                      </span>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div class="w-5/6 flex items-center justify-center m-3">
+          <button
+            className="bg-pink-400 text-pink-100 border-2 border-pink-800 shadow-2xl w-2/6 px-10 py-3 rounded-lg text-xs md:text-lg lg:text-xl transition delay-300 ease-in hover:border-pink-400 hover:bg-pink-800 hover:text-white"
+            onClick={() => {
+              history.push('/')
+            }}
+          >
+            Back to Menu
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
